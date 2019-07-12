4de80e355c70ff215283d2214754052bfdfb5ef7
nimit shukla <nimit.shukla@viseo.com>
2019-07-12 16:52:49 +0800

    added css for change

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,52 @@
 var path = require('path');
-    var ExtractTextPlugin = require('sgmf-scripts')['extract-text-webpack-plugin'];
-    var sgmfScripts = require('sgmf-scripts');
+var ExtractTextPlugin = require('sgmf-scripts')['extract-text-webpack-plugin'];
+var sgmfScripts = require('sgmf-scripts');
 
-    module.exports = [{
-        mode: 'production',
-        name: 'js',
-        entry: sgmfScripts.createJsPath(),
-        output: {
-            path: path.resolve('./cartridges/plugin_giftcertificate/cartridge/static'),
-            filename: '[name].js'
-        }
-    }, {
-        mode: 'none',
-        name: 'scss',
-        entry: sgmfScripts.createScssPath(),
-        output: {
-            path: path.resolve('./cartridges/plugin_giftcertificate/cartridge/static'),
-            filename: '[name].css'
-        },
-        module: {
-            rules: [{
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                        loader: 'css-loader',
-                        options: {
-                            url: false
-                        }
-                    }, {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                require('autoprefixer')()
-                            ]
-                        }
-                    }, {
-                        loader: 'sass-loader'
-                    }]
-                })
-            }]
-        },
-        plugins: [
-            new ExtractTextPlugin({ filename: '[name].css' })
-        ]
-    }];
+module.exports = [{
+	mode: 'production',
+	name: 'js',
+	entry: sgmfScripts.createJsPath(),
+	output: {
+		path: path.resolve('./cartridges/plugin_giftcertificate/cartridge/static'),
+		filename: '[name].js'
+	}
+}, {
+	mode: 'none',
+	name: 'scss',
+	entry: sgmfScripts.createScssPath(),
+	output: {
+		path: path.resolve('./cartridges/plugin_giftcertificate/cartridge/static'),
+		filename: '[name].css'
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+				use: [{
+					loader: 'css-loader',
+					options: {
+						url: false
+					}
+				}, {
+					loader: 'postcss-loader',
+					options: {
+						plugins: [
+							require('autoprefixer')()
+						]
+					}
+				}, {
+					loader: 'sass-loader',
+					options: {
+						includePaths: [
+							path.resolve('../storefront-reference-architecture/node_modules'),
+							path.resolve('../storefront-reference-architecture/node_modules/flag-icon-css/sass')
+						]
+					}
+				}]
+			})
+		}]
+	},
+	plugins: [
+		new ExtractTextPlugin({ filename: '[name].css' })
+	]
+}];
